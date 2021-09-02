a3aad6588d5b635254b147c5fa9a387389414b6b
zxsr <1546356192@qq.com>
2021-09-02 12:36:37 +0800

    aboutus和contac us页面

--- src/components/contactus/contactus.jsx
+++ src/components/contactus/contactus.jsx
@@ -1,10 +1,44 @@
 import React, { Component } from 'react'
+import { Link, NavLink } from "react-router-dom";
+import './contactus.css'
+import callPic from "../../assets/imgs/call.png"
+import addressPic from "../../assets/imgs/address.png";
+import emailPic from "../../assets/imgs/email.png";
 
 export default class ContactUS extends Component {
     render() {
         return (
             <div>
-                <p>ContactUS</p>
+                <h1>Contact</h1>
+                <h4 className="hrefStyle"><Link to="/booklist">Home</Link></h4>
+                <p className="descStyle">If you would like to contact CBD,you can either contact to us directly or complete the form below.Please be assured that as soon as possible.</p>
+                <table class="table table-bordered" className="tableStyle">
+                <tr className="trStyle">
+                    <th className="thTitle">
+                        <span>Head Office</span></th>
+                    <th className="thValue">
+                        <img src={callPic} className="iconStyle"/>
+                        <span>+61 3 9925 5005</span></th>
+                </tr>
+                <tr className="trStyle">
+                    <th className="thTitle">
+                        <span>Online Store</span></th>
+                    <th className="thValue">
+                        <div>
+                            <img src={emailPic} className="iconStyle" />
+                            <a href="#"><span>s3719110@student.rmit.edu.au</span></a>
+                        </div>
+                        <div>
+                            <img src={addressPic} className="iconStyle"/>
+                            <span>115 Queensberry St, Carlton VIC 3053</span>
+                        </div>
+                    </th> 
+                </tr>
+                {/* <tr className="trStyle">
+                    <td>January</td>
+                    <td>$100</td>
+                </tr> */}
+                </table>
             </div>
         )
     }
--- src/components/personal/personal.jsx
+++ src/components/personal/personal.jsx
@@ -1,10 +1,40 @@
 import React, { Component } from 'react';
-
+import './personal.css'
 class Personal extends Component {
+    state={
+        userInfo:{
+            userName:"lyric",
+            email:'1546356192@qq.com',
+            mobile:'12345678901',
+            job:'coder',
+            age:27,
+            sex:'male'
+        },
+        username:"lyric"
+    };
     render() {
         return (
             <div>
-                详情页面
+                {/* 详情页面 */}
+                <h3>{this.state.userInfo.username}</h3>
+                {/* <div className="col-sm-4 col-md-4" onClick={this.handleBuy}> */}
+                {/* <div className="col-sm-6 col-md-6 row">
+                    <label className="labelSpan">email:</label>
+                    <span>{this.state.userInfo.email}</span>
+                </div> */}
+                <div className="col-sm-4 col-md-4 col-md-offset-4">
+                    {Object.keys(this.state.userInfo).map((obj, idx) => (
+                        <div className="row">
+                            <div key={idx} className="listItemTitle">{obj} : </div> 
+                            <div className="listItemValue">{this.state.userInfo[obj]}</div>  
+                        </div>
+                              
+                    ))}
+                    <div>
+                        
+                    </div>
+                </div>
+                
             </div>
         );
     }
